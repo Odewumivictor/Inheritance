6767c83d9441cb612283893f38df8073ac4d02c7
odewumivictor <vickidubi1@gmail.com>
2022-09-08 11:41:21 +0100

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Father
+    {
+        public string FatherName()
+        {
+            return "Ronaldo";
+        }
+    }
+
+    public class ChildFirst : Father
+    {
+        public string ChildDName()
+        {
+            return "Messi";
+        }
+    }
+
+
+    public class ChildSecond : Father
+    {
+        public string ChildDName()
+        {
+            return "Dybala";
+        }
+    
+
+        static public void Main()
+        {
+            ChildFirst first = new ChildFirst();
+
+            Console.WriteLine("My name is " + first.ChildDName() +
+                              ". My father name is " +
+                              first.FatherName() + ".");
+
+            ChildSecond second = new ChildSecond();
+
+            Console.WriteLine("My name is " + second.ChildDName() +
+                              ". My father name is " +
+                              second.FatherName() + ".");
+        } 
+    }
+   
+}
